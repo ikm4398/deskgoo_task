f0cc8570802e78fecf1f5b277ff7f2c48b12589b
Indra Kumar Mehta <ikm4398@gmail.com>
2026-04-21 09:18:14 +0000

    add item in lead

---
+++ deskgoo_task/deskgoo_task/doctype/lead_followup_details_table/lead_followup_details_table.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, indra and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class LeadFollowupDetailsTable(Document):
+	pass
